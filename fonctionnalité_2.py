0564cde34f610b8bdaeab5ac22ce58b5e1247a4b
emma2110 <emma.naulet35@gmail.com>
2024-06-17 22:03:42 +0200

    Modifs pour avoir une meilleure matrice

--- fonctionnalité_2.py
+++ fonctionnalité_2.py
@@ -1,135 +1,121 @@
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                 IMPORTATIONS                  ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-import time
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix, roc_curve, auc
-import matplotlib.pyplot as plt
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-
-
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score, f1_score
+from numpy import mean
 
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃            PREPARATION DES DONNEES            ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-pd.set_option('display.max_columns', None)
+data = pd.read_csv("Data_Arbre.csv")
 
-df = pd.read_csv("Data_Arbre.csv")
+#=================================================================
+#=================== Préparation des données =====================
+#=================================================================
 
-colonnes = ["longitude","latitude","haut_tot","tronc_diam","fk_port","feuillage","fk_stadedev","fk_revetement","age_estim","fk_arb_etat"]
-data = df[colonnes].copy()
+# Mettre les noms en majuscules
+data['fk_nomtech'] = data['fk_nomtech'].str.upper()
+# Mettre les fk_stadedev en minuscules
+data['fk_stadedev'] = data['fk_stadedev'].str.lower()
 
-label_encoder = LabelEncoder()
-data['fk_revetement_encoded'] = label_encoder.fit_transform(data['fk_revetement'])
-data.drop('fk_revetement', axis=1, inplace=True)
-data['fk_port_encoded'] = label_encoder.fit_transform(data['fk_port'])
-data.drop('fk_port', axis=1, inplace=True)
-data['fk_stadedev_encoded'] = label_encoder.fit_transform(data['fk_stadedev'])
-data.drop('fk_stadedev', axis=1, inplace=True)
-data['feuillage_encoded'] = label_encoder.fit_transform(data['feuillage'])
-data.drop('feuillage', axis=1, inplace=True)
+# Transformer données texte en données numériques
+    # fk_stadedev
+enc = OrdinalEncoder()
+train_cat = data[['fk_stadedev']]
+c = enc.fit_transform(train_cat)
+data[['fk_stadedev']] = c
 
-data['fk_arb_etat'] = data['fk_arb_etat'].replace({
-    'Essouché': 1,
-    'EN PLACE': 0,
-    'SUPPRIMÉ': 0,
-    'Non essouché': 0,
-    'REMPLACÉ': 0,
-    'ABATTU': 0
-})
+    # fk_nomtech
+enc2 = OrdinalEncoder()
+train_cat2 = data[['fk_nomtech']]
+c2 = enc2.fit_transform(train_cat2)
+data[['fk_nomtech']] = c2
 
-print(data)
+# Définir des intervalles d'âge dans une nouvelle colonne 'age_group'
+bins = [0, 10, 20, 30, 40, 50, 100, 200]
+labels = [0, 1, 2, 3, 4, 5, 6]
+data['age_group'] = pd.cut(data['age_estim'], bins=bins, labels=labels, right=True)
+data = data.dropna()  # supprimer les lignes NaN
 
-# Séparation des caractéristiques (X) et de la cible (y)
-X = data.drop('fk_arb_etat', axis=1)  # Remplacez 'target_column' par le nom de votre colonne cible
-y = data['fk_arb_etat']
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                 APPRENTISSAGE                 ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+# Séparation des features X et des labels y
+X = data[['haut_tot', 'haut_tronc', 'tronc_diam', 'fk_stadedev', 'fk_nomtech']]
+y = data['age_group']
 
+# Répartition des données : 80% apprentissage, 20% test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 
-# Entraînement du modèle
-rf_classifier.fit(X_train, y_train)
-
-# Prédiction sur l'ensemble de test
-y_pred = rf_classifier.predict(X_test)
+# Normaliser les données
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃               ANALYSE RESULTATS               ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+# Afficher les associations valeurs numériques / valeurs textuelles
+#for i, category in enumerate(enc.categories_[0]):
+    #print(f"  {category} -> {i}")
+
+#for i, category in enumerate(enc2.categories_[0]):
+    #print(f"  {category} -> {i}")
 
 
-# Rapport de classification
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy : {accuracy:.2f}')
-report = classification_report(y_test, y_pred, zero_division=0)
-print(report)
 
-# Matrice de confusion
-conf_matrix = confusion_matrix(y_test, y_pred)
-print(conf_matrix)
+#=================================================================
+#=================== Apprentissage supervisé =====================
+#=================================================================
 
-# Affichage de la matrice de confusion
-plt.figure(figsize=(10, 7))
-sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues")
-plt.xlabel('Prédiction')
-plt.ylabel('Vérité terrain')
+#============= Stochastic Gradient Descent (SGD) =================
+
+classifier = SGDClassifier()
+classifier.fit(X_train, y_train)
+
+
+#=======================================================================
+#=================== Evaluation via des métriques  =====================
+#=======================================================================
+
+
+#=================== Taux de classification ============================
+
+y_pred = classifier.predict(X_test)
+
+# Tableau des taux de classification
+folds = cross_val_score(classifier, X_test, y_test, scoring="accuracy", cv=5)
+print("Taux de classification : ", folds)
+
+# moyenne des taux de classification
+print("Moyenne des taux : ", mean(folds))
+
+# taux de classification avec normalisation
+#folds2 = cross_val_score(classifier, test_data, test_labels, scoring="accuracy", cv=5)
+#print(folds2)
+#print(mean(folds2))
+
+
+#=================== Matrice de confusion ==============================
+
+mc = confusion_matrix(y_test, y_pred)
+
+# afficher matrice avec des pourcentages
+disp = ConfusionMatrixDisplay.from_predictions(y_test, y_pred, normalize='true', values_format=".0%")
 plt.title('Matrice de Confusion')
 plt.show()
 
-y_pred_proba = rf_classifier.predict_proba(X_test)[:, 1]
-
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
-roc_auc = auc(fpr, tpr)
-
-# Tracer la courbe ROC
-plt.figure(figsize=(8, 6))
-plt.plot(fpr, tpr, color='blue', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], color='gray', linestyle='--', lw=2)
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('Taux de faux positifs (FPR)')
-plt.ylabel('Taux de vrais positifs (TPR)')
-plt.title('Courbe ROC')
-plt.legend(loc="lower right")
-plt.show()
 
 
-# Définir la grille des hyperparamètres à tester
-param_grid = {
-    'n_estimators': [50, 100, 200],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': ['auto', 'sqrt', 'log2']
-}
+#=================== Précision, rappel, f1 score ===================================
 
-# Créer l'objet GridSearchCV
-grid_search = GridSearchCV(estimator=rf_classifier, param_grid=param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+# précision de chaque classe
+precision = precision_score(y_test, y_pred, average=None)
+print("Précision : ", precision)
 
-# Exécuter la recherche sur grille sur les données d'entraînement
-grid_search.fit(X_train, y_train)
+# rappel de chaque classe
+rappel = recall_score(y_test, y_pred, average=None)
+print("Rappel : ", rappel)
 
-# Afficher les meilleurs paramètres et le meilleur score
-print("Meilleurs paramètres trouvés :")
-print(grid_search.best_params_)
-print("Meilleur score d'entraînement :")
-print(grid_search.best_score_)
-
-# Prédire sur l'ensemble de test avec le meilleur modèle trouvé
-y_pred = grid_search.best_estimator_.predict(X_test)
-
-# Évaluer la précision du meilleur modèle sur l'ensemble de test
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Précision sur l'ensemble de test : {accuracy:.2f}")
+# f1 score de chaque classe
+f1score = f1_score(y_test, y_pred, average=None)
+print("F1 Score : ", f1score)
