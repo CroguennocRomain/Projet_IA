eff8171b0b94b7e175a947f9948d878f50e9837a
romain.cro <romain.croguennoc@isen-ouest.yncrea.fr>
2024-06-18 16:32:16 +0200

    script python + finalisation de fonc3

--- fonctionnalité_3.py
+++ fonctionnalité_3.py
@@ -2,10 +2,9 @@
 # ┃                 IMPORTATIONS                  ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-import time
-import sys
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix, roc_curve, auc
 import matplotlib.pyplot as plt
@@ -24,34 +23,43 @@ pd.set_option('display.max_columns', None)
 df = pd.read_csv("Data_Arbre.csv")
 
 
-colonnes = ["haut_tot","tronc_diam","fk_port","feuillage","fk_stadedev","fk_revetement","age_estim","fk_arb_etat"]
+colonnes = ["latitude","longitude","haut_tot","haut_tronc","feuillage","fk_stadedev","fk_revetement","age_estim","fk_arb_etat","fk_pied","fk_situation",'remarquable']
 data = df[colonnes].copy()
 
 label_encoder = LabelEncoder()
 data['fk_revetement_encoded'] = label_encoder.fit_transform(data['fk_revetement'])
-data['fk_port_encoded'] = label_encoder.fit_transform(data['fk_port'])
+data['fk_situation_encoded'] = label_encoder.fit_transform(data['fk_situation'])
 data['fk_stadedev_encoded'] = label_encoder.fit_transform(data['fk_stadedev'])
+data['remarquable_encoded'] = label_encoder.fit_transform(data['remarquable'])
+
 data['feuillage_encoded'] = label_encoder.fit_transform(data['feuillage'])
+data['fk_pied_encoded'] = label_encoder.fit_transform(data['fk_pied'])
+
 
 data['fk_arb_etat'] = data['fk_arb_etat'].replace({
     'Essouché': 1,
     'EN PLACE': 0,
     'SUPPRIMÉ': 0,
-    'Non essouché': 0,
+    'Non essouché': 1,
     'REMPLACÉ': 0,
     'ABATTU': 0
 })
 
-print(data)
-
-# Séparation des caractéristiques (X) et de la cible (y)
-X = data[['haut_tot', 'tronc_diam', 'age_estim', 'feuillage_encoded', 'fk_revetement_encoded', 'fk_port_encoded', 'fk_stadedev_encoded']]
+# X ET Y POUR RANDOM FOREST
+X = data[["haut_tronc","latitude","longitude",'fk_stadedev_encoded','haut_tot']]
 y = data['fk_arb_etat']
+"""
+# X ET Y POUR KNN
+X = data[["haut_tronc","latitude","longitude",'fk_stadedev_encoded','haut_tot','fk_revetement_encoded','fk_situation_encoded','fk_pied_encoded','age_estim']]
+y = data['fk_arb_etat']
+"""
+
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                APPRENTISSAGE 1                ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 
@@ -61,6 +69,19 @@ rf_classifier.fit(X_train, y_train)
 # Prédiction sur l'ensemble de test
 y_pred = rf_classifier.predict(X_test)
 
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                APPRENTISSAGE 2                ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Entraînement du modèle KNN
+knn_classifier = KNeighborsClassifier(n_neighbors=5)
+knn_classifier.fit(X_train, y_train)
+
+# Prédiction sur l'ensemble de test
+y_pred = knn_classifier.predict(X_test)
+"""
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃               ANALYSE RESULTATS               ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
@@ -86,6 +107,7 @@ plt.show()
 
 y_pred_proba = rf_classifier.predict_proba(X_test)[:, 1]
 
+#y_pred_proba = knn_classifier.predict_proba(X_test)[:, 1]
 #------------------------Courbe ROC---------------------------
 fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
 roc_auc = auc(fpr, tpr)
@@ -103,7 +125,7 @@ plt.show()
 
 #-------------------------- GRID-SCV--------------------------
 # Définir la grille des hyperparamètres à tester
-param_grid = {
+"""param_grid = {
     'n_estimators': [50, 100, 200],
     'max_depth': [None, 10, 20],
     'min_samples_split': [2, 5, 10],
@@ -133,3 +155,4 @@ print(f"Précision sur l'ensemble de test : {accuracy:.2f}")
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                APPRENTISSAGE 2                ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
