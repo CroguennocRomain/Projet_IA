328abf3274cf320de2eb1b567c439acbc3664301
emma2110 <emma.naulet35@gmail.com>
2024-06-19 09:51:05 +0200

    début adaptation script pour 1 arbre

--- script_age.py
+++ script_age.py
@@ -41,19 +41,49 @@ def predire_age(features, method):
     # Application du modèle à nos données
     model.fit(X_train, y_train)
 
+    # Prédictions
     y_pred = model.predict(X_test)
     json_data = json.dumps(y_pred.tolist())
 
     return json_data
 
 
+def predire_age2(values, method):
+
+    # associer noms de colonnes et valeurs dans une structure json
+    data = {}
+    if len(values) == 5:
+        data['haut_tot'] = values[0]
+        data['haut_tronc'] = values[1]
+        data['tronc_diam'] = values[2]
+        data['fk_stadedev'] = values[3]
+        data['nomfrancais'] = values[4]
+    else:
+        print("Erreur : Il faut entrer 6 arguments")
+
+    # Sélection du modèle d'apprentissage
+    if method == '0':
+        model = SGDClassifier()
+    elif method == '1':
+        model = KNeighborsClassifier(n_neighbors=3)
+    elif method == '2':
+        model = svm.SVC()
+    elif method == '3':
+        model = tree.DecisionTreeClassifier()
+
+    print(data)
+    print(model)
+
+    return 0
+
+
 # On récupère les arguments noms de colonnes dans features
 features = []
-# On récupère l'indice du dernier argument corresponndant au modèle
+# On récupère l'indice du dernier argument correspondant au modèle
 i_last_arg = len(sys.argv) - 1  # -1 car on ne compte pas argv[0] qui est le nom du script
 for i in range(1, i_last_arg):
     features.append(sys.argv[i])
 
 
-age = predire_age(features, sys.argv[i_last_arg])
-print(age)
+predire_age2(features, sys.argv[i_last_arg])
+#print(age)
