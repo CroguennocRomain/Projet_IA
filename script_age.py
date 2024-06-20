c4800305913dab8238fe10ae4ce3bb916b8ff679
emma2110 <emma.naulet35@gmail.com>
2024-06-20 11:49:48 +0200

    script age avec encoder (sans scaler)

--- script_age.py
+++ script_age.py
@@ -12,30 +12,63 @@ import pickle
 
 
 def predire_age(values, method):
+    # Charger l'encodeur depuis le fichier
+    with open('ordinal_encoder.pkl', 'rb') as file:
+        encoder = pickle.load(file)
 
-    # associer noms de colonnes et valeurs dans une structure json
-    data = {}
+    # Colonnes utilisées lors de l'entraînement du OrdinalEncoder
+    encoder_cols = encoder.feature_names_in_
+
+    # Ajouter valeurs entrées dans un dictionnaire
+    data_input = {}
     if len(values) == 5:
-        data['haut_tot'] = values[0]
-        data['haut_tronc'] = values[1]
-        data['tronc_diam'] = values[2]
-        data['fk_stadedev'] = values[3]
-        data['nomfrancais'] = values[4]
+        data_input['haut_tot'] = values[0]
+        data_input['haut_tronc'] = values[1]
+        data_input['tronc_diam'] = values[2]
+        data_input['fk_stadedev'] = values[3]
+        data_input['fk_nomtech'] = values[4]
     else:
         print("Erreur : Il faut entrer 6 arguments")
 
-    # Créer l'instance au bon format
-    arbre = np.array([[data["haut_tot"], data["haut_tronc"], data["tronc_diam"], data["fk_stadedev"], data["nomfrancais"]]])
+    # Convertir le dictionnaire en DataFrame
+    df = pd.DataFrame([data_input])
 
-    # Transformer valeurs catégorielles en numériques
-    with open('ordinal_encoder.pkl', 'rb') as f:
-        encoder = pickle.load(f)
-    arbre[0][3] = encoder.transform([[arbre[0][3]]])[0]
-    arbre[0][4] = encoder.transform([[arbre[0][4]]])[0]
+    data_arbre = pd.read_csv('Data_Arbre.csv')
+    # Ajouter les colonnes manquantes avec des valeurs NaN
+    for col in encoder_cols:
+        if col not in df.columns:
+            df[col] = data_arbre[col][0]
+
+    # Réorganiser les colonnes selon l'ordre des colonnes utilisées lors de l'entraînement
+    df = df[encoder_cols]
+
+    # Remplir les valeurs manquantes avec une valeur par défaut
+    df.fillna('missing_value', inplace=True)
+
+    # Encoder les colonnes textuelles en numériques
+    df[encoder_cols] = encoder.transform(df[encoder_cols])
+
+    # Supprimer les colonnes qui ne nous intéressent pas
+    df.drop(columns=['clc_quartier', 'clc_secteur', 'fk_arb_etat', 'fk_port',
+                     'fk_pied', 'fk_situation', 'fk_revetement', 'villeca',
+                     'feuillage', 'remarquable'], inplace=True)
+
+    # Ajouter les colonnes non encodées
+    for col in ['haut_tot', 'haut_tronc', 'tronc_diam']:
+        df[col] = data_input[col]
+
+
+    # Mettre notre instance à prédire sous le bon format
+    arbre = np.array([[float(df['haut_tot'][0]), float(df['haut_tronc'][0]), float(df['tronc_diam'][0]), float(df['fk_stadedev'][0]), float(df['fk_nomtech'][0])]])
+    print(arbre)
 
     # Normalisation
-    scaler = StandardScaler()
-    arbre = scaler.fit_transform(arbre)
+    """
+    with open("normalisation.pkl", "rb") as f:
+        scaler = pickle.load(f)
+    df_norm = scaler.fit_transform(df)
+    print(df_norm)
+    """
 
     # Sélection du modèle d'apprentissage
     if method == '0':
@@ -63,7 +96,7 @@ def predire_age(values, method):
     json_data['41-50'] = classes[0][4]
     json_data['51-100'] = classes[0][5]
     json_data['101-200'] = classes[0][6]
-
+    
     # Renvoie les données en format json
     return json.dumps(json_data)
 
